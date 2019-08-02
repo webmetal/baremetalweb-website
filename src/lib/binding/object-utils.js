403e99fe2071f1bcbba2350a6aa5e8d346bb0b0e
Mental Arena <caperaven@icloud.com>
2019-08-02 17:14:47 +0200

    fixed the this operator on delegates on paths

--- src/lib/binding/object-utils.js
+++ src/lib/binding/object-utils.js
@@ -1,3 +1,10 @@
+/**
+ * Get a property value on a given path of object and sub objects
+ * @param obj {Object}: The starting point fo the search
+ * @param path {String}: The property path that the value is found on
+ * @param includeParent {Boolean}: Should you include the parent object in the result, if not only the property is passed back
+ * @returns {Promise<{parent: Object, value: Object}>}
+ */
 export async function getValueOnPath(obj, path, includeParent = false) {
     const parts = path.split(".");
     let parent = obj;
