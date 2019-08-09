f47911f3b6ceff04295e477cabb13874aecf2bc7
Mental Arena <caperaven@icloud.com>
2019-08-09 11:55:55 +0200

    template inflation

--- src/lib/templates/templates.js
+++ src/lib/templates/templates.js
@@ -1,15 +1,21 @@
 /**
- * Get template based on parts requested
- * @param name
- * @param parts
+ * Template part constants
+ * @type {{css: number, js: number, html: number}}
  */
-
 export const tp = {
     js: 1,
     html: 2,
     css: 4
 };
 
+/**
+ * This function loads template parts and sends it back tot he caller as a composite
+ * @param type {string}: folder that templates are in
+ * @param name {string}: name of the template
+ * @param parts {bitmap}: what templates to load
+ * @param content {object}: object defining the content parts to inflate
+ * @returns {Promise<void>}
+ */
 export async function getTemplates(type, name, parts, content) {
     const result = {};
     result[tp.js] = parts & tp.js && await fetchTemplateContent(type, name, "js", content);
@@ -18,11 +24,21 @@ export async function getTemplates(type, name, parts, content) {
     return result;
 }
 
+/**
+ * This function fetches the content and inflates the content
+ * @param type {string}: the type of content to search in, determines the sub folders
+ * @param name {string}: the name of the template file
+ * @param ext {string}: extension of the file being loaded
+ * @param content {object}: the object used to inflate the content
+ * @returns {Promise<string>}
+ */
 async function fetchTemplateContent(type, name, ext, content) {
     let result = await fetch(`./../../templates/${type}/${name}.${ext}`).then(result => result.text());
 
-    for (const [key, value] of Object.entries(content)) {
-        result = result.split(key).join(value);
+    if (content != null) {
+        for (const [key, value] of Object.entries(content)) {
+            result = result.split(key).join(value);
+        }
     }
 
     return result;
