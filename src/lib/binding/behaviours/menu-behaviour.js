0fa75a93bbe91663a8d331a66fc6feb97f2b0798
Mental Arena <caperaven@icloud.com>
2019-08-24 00:32:53 +0200

    menu behaviour update

--- src/lib/binding/behaviours/menu-behaviour.js
+++ src/lib/binding/behaviours/menu-behaviour.js
@@ -4,14 +4,19 @@ export class MenuBehaviour extends BaseBehaviour {
     constructor(attribute, context) {
         super(attribute, context);
         this.updateCollectionHandler = this._updateCollection.bind(this);
+        this.clickHandler = this._click.bind(this);
         this._initialize();
     }
 
     dispose() {
-        super.dispose();
+        this._attribute.ownerElement.removeEventListener("click", this.clickHandler);
         this.updateCollectionHandler = null;
         this.displayPro = null;
         this.collectionProp = null;
+        this.eventCallback = null;
+        this.clickHandler = null;
+        this.items = null;
+        super.dispose();
     }
 
     _initialize() {
@@ -19,8 +24,10 @@ export class MenuBehaviour extends BaseBehaviour {
         const bindProperty = markup[0];
         this.collectionProp = markup[1];
         this.displayPro = markup[2];
+        this.eventCallback = markup[3];
 
-        this._context.on(bindProperty, this.updateCollectionHandler)
+        this._context.on(bindProperty, this.updateCollectionHandler);
+        this._attribute.ownerElement.addEventListener("click", this.clickHandler);
     }
 
     async _updateCollection(prop, value) {
@@ -35,9 +42,16 @@ export class MenuBehaviour extends BaseBehaviour {
     }
 
     async _updateArray(array) {
+        this.items = array;
         if (array == null) return;
 
+        return this._renderItems(array);
+    }
+
+    async _renderItems(array, backItem) {
         const fragment = document.createDocumentFragment();
+        backItem && fragment.appendChild(backItem);
+
         for (let item of array) {
             const listItem = await this._createMenuItem(item);
             fragment.appendChild(listItem);
@@ -53,6 +67,7 @@ export class MenuBehaviour extends BaseBehaviour {
         const li = document.createElement("li");
         li.setAttribute("role", "menuitem");
         li.setAttribute("data-count", count);
+        li.setAttribute("data-path", item.id);
         li.innerText = item[this.displayPro];
 
         if (count > 0) {
@@ -60,5 +75,35 @@ export class MenuBehaviour extends BaseBehaviour {
         }
 
         return li;
+    }
+
+    async _click(event) {
+        const target = event.target;
+        const path = target.dataset.path;
+        if (target.dataset.count && target.dataset.count > 0) {
+            const result = await this._itemsOnPath(path, this.items);
+            return this._renderItems(result);
+        }
+
+        const result = await this._itemsOnPath(path, this.items);
+        this._context[this.eventCallback](result);
+    }
+
+    async _itemsOnPath(path, collection) {
+        const parts = path.split(".");
+        if (parts.length == 1) {
+            return collection.find(item => item.id == path)[this.collectionProp];
+        }
+
+        let id = "";
+        let item;
+        for (let i = 0; i < parts.length; i++) {
+            id = id + parts[i];
+            item = collection.find(item => item.id == id);
+            collection = item[this.collectionProp];
+            id += "."
+        }
+
+        return item;
     }
 }
