dff617d897a57fbe910eae516edcf0f1f18814bf
Mental Arena <caperaven@icloud.com>
2019-08-28 23:33:41 +0200

    added monaco editor

--- src/monaco-editor/monaco-editor.js
+++ src/monaco-editor/monaco-editor.js
@@ -1,7 +1,37 @@
 class MonacoEditor extends HTMLElement {
+    get frame() {
+        if (this._frame == null) {
+            this._frame = this.querySelector("iframe");
+        }
+        return this._frame;
+    }
+
+    set frame(newValue) {
+        this._frame = newValue;
+    }
+
+    get language() {
+        return this._language || this.getAttribute("language");
+    }
+
+    set language(newValue) {
+        this._language = newValue;
+        this.frame.contentWindow.postMessage({language: newValue});
+    }
+
     async connectedCallback() {
         this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
-        this.querySelector("iframe").setAttribute("src", import.meta.url.replace(".js", ".tpl.html"));
+        this.frame.onload = () => {
+            this.language = "javascript";
+            this.frame.onload = null;
+        };
+        this.frame.setAttribute("src", import.meta.url.replace(".js", ".tpl.html"));
+
+    }
+
+    disconnectedCallback() {
+        this.frame = null;
+        this.innerHTML = null;
     }
 }
 
